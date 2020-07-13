d5d4462e6da54ac625f8b97ff137d5111982aa40
Felipe Martins <felipemjesus@outlook.com>
2020-07-13 20:51:29 -0300

    Componente com filhos

--- src/index.js
+++ src/index.js
@@ -2,18 +2,27 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './components/Primeiro'
-import ComParamentro from './components/ComParamentro'
+// import Primeiro from './components/Primeiro'
+// import ComParamentro from './components/ComParamentro'
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render(
     <>
-        <Primeiro />
+        <ComFilhos>
+            <ul>
+                <li>Ana</li>
+                <li>Bia</li>
+                <li>Carlos</li>
+                <li>Daniel</li>
+            </ul>
+        </ComFilhos>
+        {/* <Primeiro />
         <ComParamentro
             titulo="Esse é o título"
             subtitulo="Esse é o subtítulo" />
         <ComParamentro
             titulo="Opa"
-            subtitulo="Epa" />
+            subtitulo="Epa" /> */}
     </>,
     document.getElementById('root')
 )
