928e4a411329e6b943fc3830b9c1ce9df8c41bac
Felipe Martins <felipemjesus@outlook.com>
2020-07-13 20:45:21 -0300

    Componente com paramentros

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Primeiro from './components/Primeiro'
+import ComParamentro from './components/ComParamentro'
 
 ReactDOM.render(
-    <Primeiro></Primeiro>,
+    <>
+        <Primeiro />
+        <ComParamentro
+            titulo="Esse é o título"
+            subtitulo="Esse é o subtítulo" />
+    </>,
     document.getElementById('root')
 )
